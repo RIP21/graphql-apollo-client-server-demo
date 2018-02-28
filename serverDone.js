b101b081c9ab5e425b05d7625acfe4b58c991a84
Andrii Los <andrii.los@revolut.com>
2018-02-28 18:10:06 +0100

    - More advanced example

--- serverDone.js
+++ serverDone.js
@@ -12,72 +12,118 @@ let books = [
   {
     id: "fdda765f-fc57-5604-a269-52a7df8164ec",
     title: "Harry Potter and the Sorcerer's stone",
-    author: "J.K. Rowling"
+    authorId: "1"
   },
   {
     id: "fdda765f-fc57-5604-a269-52afrgthy4ec",
     title: "Jurassic Park",
-    author: "Michael Crichton"
+    authorId: "2"
   }
+];
+
+let authors = [
+  { id: "1", name: "J.K. Rowling" },
+  { id: "2", name: "Michael Crichton" }
 ];
 
 // The GraphQL schema in string form
 const typeDefs = gql`
-  type Query {
-    books: [Book]
-    book(title: String!): Book
-  }
-
-  type Mutation {
-    addBook(title: String!, author: String): Book
-    deleteBook(id: String!): Message
-    updateBook(id: String!, changes: UpdateBook!): Book
+  input AuthorInput {
+    name: String!
   }
 
   input UpdateBook {
     title: String
-    author: String
-  }
-
-  type Message {
-    deletedId: ID
-    message: String!
+    authorId: ID
+    author: AuthorInput
   }
 
   type Book {
     id: ID!
     title: String
-    author: String
+    author: Author
+  }
+
+  type Author {
+    id: ID!
+    name: String!
+  }
+
+  type Query {
+    books: [Book]
+    book(id: ID!): Book
+    authors: [Author]
+    author(id: ID!): Author
+  }
+
+  type Mutation {
+    addBook(title: String!, authorId: String, author: AuthorInput): Book
+    deleteBook(id: String!): ID
+    updateBook(id: String!, changes: UpdateBook!): Book
+    addAuthor(name: String!): Author
   }
 `;
+
+const addAuthor = ({ name }) => {
+  const author = { id: uuid(), name };
+  authors.push(author);
+  return author;
+};
 
 // The resolvers
 const resolvers = {
   Query: {
-    book: (_, { title }) => books.find(book => book.title === title),
-    books: () => books
+    book(_, { id }) {
+      return books.find(book => book.id === id);
+    },
+    books() {
+      return books;
+    },
+    authors() {
+      return authors;
+    },
+    author(root, { id }) {
+      return authors.find(author => author.id === id);
+    }
+  },
+  Book: {
+    author({ authorId }) {
+      return authors.find(author => author.id === authorId);
+    }
   },
   Mutation: {
-    addBook: (_, book) => {
-      book.id = uuid();
+    addBook(_, { title, authorId, author }) {
+      let book = { id: uuid(), title };
+      if (authorId) {
+        book.authorId = authorId;
+        books.push(book);
+        return book;
+      }
+      book.authorId = addAuthor(author).id;
       books.push(book);
       return book;
     },
-    deleteBook: (_, { id }) => {
+    deleteBook(_, { id }) {
       const preLength = books.length;
       books = books.filter(book => book.id !== id);
-      return preLength > books.length
-        ? { deletedId: id, message: "Success!" }
-        : new Error("No book to delete!");
+      return preLength > books.length ? id : new Error("No book to delete!");
     },
-    updateBook: (_, { id, changes }) => {
+    updateBook(_, { id, changes: { title, author, authorId } }) {
       const bookToUpdate = books.find(book => book.id === id);
       if (bookToUpdate) {
-        const newBook = { ...bookToUpdate, ...changes };
+        const newBook = { ...bookToUpdate, title };
+        if (authorId) {
+          newBook.authorId = authorId;
+        } else {
+          newBook.authorId = addAuthor(author).id;
+        }
         books = [...books.filter(book => book.id !== id), newBook];
         return newBook;
       }
       return new Error("No book to update!");
+    },
+    addAuthor(root, { name }) {
+      return addAuthor({ name });
     }
   }
 };
